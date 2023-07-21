14608fbd56b36646536de7876ef8f66b291cd0a8
gkiplangat <gideonkiplangat4@gmail.com>
2023-07-21 04:26:36 -0700

    fix:betty style

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,35 +12,34 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    int *prev, *next, temp;
-    int swap_occurred;
-    size_t index, pass;
+	int *prev, *next, temp;
+	int swap_occurred;
+	size_t index, pass;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+	return;
 
-    for (pass = 0; pass < size - 1; pass++)
-    {
-        prev = array;
-        next = array + 1;
-        swap_occurred = false;
+	for (pass = 0; pass < size - 1; pass++)
+	{
+		prev = array;
+		next = array + 1;
+		swap_occurred = false;
 
-        for (index = 0; index < size - pass - 1; index++)
-        {
-            if (*prev > *next)
-            {
-                temp = *prev;
-                *prev = *next;
-                *next = temp;
-                swap_occurred = true;
-                print_array(array, size);
-            }
-            prev = next;
-            next = array + index + 2;
-        }
-
-        if (!swap_occurred)
-            break;
-    }
+		for (index = 0; index < size - pass - 1; index++)
+		{
+		       if (*prev > *next)
+		       {
+			       temp = *prev;
+			       *prev = *next;
+			       *next = temp;
+			       swap_occurred = true;
+			       print_array(array, size);
+		       }
+		       prev = next;
+		       next = array + index + 2;
+		}
+		if (!swap_occurred)
+		break;
+	}
 }
 
