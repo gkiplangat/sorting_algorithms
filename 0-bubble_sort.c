92ea2cca67cc845c63cae856721556fada5f7038
gkiplangat <gideonkiplangat4@gmail.com>
2023-07-21 07:24:13 -0700

    fix:betty errors

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,41 +4,37 @@
  * bubble_sort - Sorting Algorithm
  * @array: The array to be sorted.
  * @size: The size of the array to be sorted.
- *
- * The bubble-sort algorithm for sorting an array of integers.
- *
  * Return: Nothing.
  */
 
 void bubble_sort(int *array, size_t size)
 {
-    int *prev, *next, temp;
-    int swap_occurred;
-    size_t index;
+	int *prev, *next, temp;
+	int swap_occurred;
+	size_t index;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+	return;
 
-    do
-    {
-        index = 1;
-        prev = array;
-        next = array + index;
-        swap_occurred = false;
+	do {
+		index = 1;
+		prev = array;
+		next = array + index;
+		swap_occurred = false;
 
-        while (index < size)
-        {
-            if (*prev > *next)
-            {
-                temp = *prev;
-                *prev = *next;
-                *next = temp;
-                swap_occurred = true;
-                print_array(array, size);
-            }
-            index++;
-            prev = next;
-            next = array + index;
-        }
-    } while (swap_occurred == true);
+		while (index < size)
+		{
+			if (*prev > *next)
+			{
+				temp = *prev;
+				*prev = *next;
+				*next = temp;
+				swap_occurred = true;
+				print_array(array, size);
+			}
+			index++;
+			prev = next;
+			next = array + index;
+		}
+	} while (swap_occurred == true);
 }
