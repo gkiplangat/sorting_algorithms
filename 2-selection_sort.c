af5400a15973e9b8f437718d203c98e9801541bc
Chelagat-Pauline-Gechure <cpaulineg@gmail.com>
2023-07-24 04:33:50 -0700

    2. Selection sort first draft solution

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,4 +9,33 @@
 
 void selection_sort(int *array, size_t size)
 {
-	/* Validate if list pointer is not null & size < 2 */
+	size_t i, j, min_idx;
+
+	/* Validate if list pointer is not null & size is less than 2 */
+	if (!array || size < 2)
+		return;
+
+	for (i = 0; i < size -1;i++)
+	{
+		/* Assume current element is the minimum value */
+		min_idx = i;
+
+		/* Search for minimum element in unsorted array */
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
+
+		/* Swap found minimum element with first unsorted element */
+		if (min_idx != i)
+		{
+			int temp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = temp;
+
+			/* Display array's current state */
+			print_array(array, size);
+		}
+	}
+}
